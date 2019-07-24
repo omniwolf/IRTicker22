1930384196ed328b6df48e1a3a49d243f3e947e8
omniwolf <seventh.adept@gmail.com>
2019-07-24 23:49:46 +1000

    - i now buffer every incoming order, and apply them to the OB on a set schedule - schedule can be edited real-time

--- IRTicker2/Form1.Designer.cs
+++ IRTicker2/Form1.Designer.cs
@@ -29,14 +29,18 @@
             this.best_bid_stat_label = new System.Windows.Forms.Label();
             this.best_bid_dyn_label = new System.Windows.Forms.Label();
             this.best_offer_dyn_label = new System.Windows.Forms.Label();
+            this.UITimer = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timerSleep = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // spread_dyn_label
             // 
             this.spread_dyn_label.AutoSize = true;
+            this.spread_dyn_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.spread_dyn_label.Location = new System.Drawing.Point(270, 48);
             this.spread_dyn_label.Name = "spread_dyn_label";
-            this.spread_dyn_label.Size = new System.Drawing.Size(0, 13);
+            this.spread_dyn_label.Size = new System.Drawing.Size(0, 25);
             this.spread_dyn_label.TabIndex = 0;
             // 
             // spread_stat_label
@@ -69,32 +73,65 @@
             // best_bid_dyn_label
             // 
             this.best_bid_dyn_label.AutoSize = true;
-            this.best_bid_dyn_label.Location = new System.Drawing.Point(117, 119);
+            this.best_bid_dyn_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.best_bid_dyn_label.Location = new System.Drawing.Point(92, 119);
             this.best_bid_dyn_label.Name = "best_bid_dyn_label";
-            this.best_bid_dyn_label.Size = new System.Drawing.Size(13, 13);
+            this.best_bid_dyn_label.Size = new System.Drawing.Size(24, 25);
             this.best_bid_dyn_label.TabIndex = 4;
             this.best_bid_dyn_label.Text = "0";
             // 
             // best_offer_dyn_label
             // 
             this.best_offer_dyn_label.AutoSize = true;
-            this.best_offer_dyn_label.Location = new System.Drawing.Point(247, 119);
+            this.best_offer_dyn_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.best_offer_dyn_label.Location = new System.Drawing.Point(222, 119);
             this.best_offer_dyn_label.Name = "best_offer_dyn_label";
-            this.best_offer_dyn_label.Size = new System.Drawing.Size(13, 13);
+            this.best_offer_dyn_label.Size = new System.Drawing.Size(24, 25);
             this.best_offer_dyn_label.TabIndex = 5;
             this.best_offer_dyn_label.Text = "0";
+            // 
+            // UITimer
+            // 
+            this.UITimer.WorkerReportsProgress = true;
+            this.UITimer.WorkerSupportsCancellation = true;
+            this.UITimer.DoWork += new System.ComponentModel.DoWorkEventHandler(this.UITimer_DoWork);
+            this.UITimer.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.UITimer_ProgressChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "set new timer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timerSleep
+            // 
+            this.timerSleep.Location = new System.Drawing.Point(110, 203);
+            this.timerSleep.Mask = "0000000000";
+            this.timerSleep.Name = "timerSleep";
+            this.timerSleep.Size = new System.Drawing.Size(100, 20);
+            this.timerSleep.TabIndex = 8;
+            this.timerSleep.Text = "100";
+            this.timerSleep.ValidatingType = typeof(int);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.timerSleep);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.best_offer_dyn_label);
             this.Controls.Add(this.best_bid_dyn_label);
             this.Controls.Add(this.best_bid_stat_label);
             this.Controls.Add(this.best_offer_stat_label);
             this.Controls.Add(this.spread_stat_label);
             this.Controls.Add(this.spread_dyn_label);
+            this.MaximumSize = new System.Drawing.Size(400, 400);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -110,6 +147,9 @@
         private System.Windows.Forms.Label best_bid_stat_label;
         private System.Windows.Forms.Label best_bid_dyn_label;
         private System.Windows.Forms.Label best_offer_dyn_label;
+        private System.ComponentModel.BackgroundWorker UITimer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox timerSleep;
     }
 }
 
